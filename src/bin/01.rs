f7b731baa056263cecf35d03537aa4f78946d6c8
JGDoerrer <jg.doerrer@gmail.com>
2022-12-02 13:04:12 +0100

    day 1

---
+++ src/bin/01.rs
@@ -0,0 +1,53 @@
+pub fn part_one(input: &str) -> Option<u32> {
+    let mut max = 0;
+    let mut current = 0;
+
+    for line in input.lines() {
+        match line.trim().parse::<u32>() {
+            Ok(v) => current += v,
+            _ => {
+                max = max.max(current);
+                current = 0;
+            }
+        }
+    }
+
+    Some(current)
+}
+
+pub fn part_two(input: &str) -> Option<u32> {
+    let mut elves = vec![0];
+
+    for line in input.lines() {
+        match line.trim().parse::<i32>() {
+            Ok(v) => *elves.last_mut().unwrap() += v,
+            _ => elves.push(0),
+        }
+    }
+
+    elves.sort();
+    Some(elves.iter().rev().take(3).sum::<i32>() as u32)
+}
+
+fn main() {
+    let input = &advent_of_code::read_file("inputs", 1);
+    advent_of_code::solve!(1, part_one, input);
+    advent_of_code::solve!(2, part_two, input);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_one() {
+        let input = advent_of_code::read_file("examples", 1);
+        assert_eq!(part_one(&input), None);
+    }
+
+    #[test]
+    fn test_part_two() {
+        let input = advent_of_code::read_file("examples", 1);
+        assert_eq!(part_two(&input), None);
+    }
+}
