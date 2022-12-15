5f265735e11ed5fb3cb03ad5cea1935095c7bbef
JGDoerrer <jg.doerrer@gmail.com>
2022-12-15 15:43:25 +0100

    optimize

--- src/bin/15.rs
+++ src/bin/15.rs
@@ -24,16 +24,12 @@ pub fn part_one(input: &str) -> Option<u32> {
 
     let target_y = if sensors.len() < 15 { 10 } else { 2000000 };
 
-    let sensors_a = sensors
+    let ranges = sensors
         .iter()
         .zip(distances.iter())
         .filter(|(((_, y), _), dist)| (target_y - y).abs() < **dist)
-        .collect_vec();
-
-    let ranges = sensors_a
-        .iter()
         .map(|(((x, y), _), dist)| {
-            let range = **dist - (target_y - y).abs();
+            let range = *dist - (target_y - y).abs();
             (x - range, x + range)
         })
         .collect_vec();
@@ -41,31 +37,14 @@ pub fn part_one(input: &str) -> Option<u32> {
     let mut new_ranges = vec![];
 
     for (mut a, mut b) in ranges {
-        loop {
-            let mut overlap = false;
-            let mut index = 0;
-
-            for (i, (c, d)) in new_ranges.iter().enumerate() {
-                if (a <= *c && b >= *c) || (*c <= a && *d >= a) {
-                    overlap = true;
-                    index = i;
-                    break;
-                }
-            }
-
-            if !overlap {
-                new_ranges.push((a, b));
-                break;
-            } else {
-                let (c, d) = new_ranges.remove(index);
-
-                if a <= c && b >= c {
-                    (a, b) = (a, b.max(d));
-                } else {
-                    (a, b) = (c, b.max(d));
-                }
-            }
+        while let Some(index) = new_ranges
+            .iter()
+            .position(|(c, d)| (a <= *c && b >= *c - 1) || (*c <= a && *d >= a - 1))
+        {
+            let (c, d) = new_ranges.remove(index);
+            (a, b) = (a.min(c), b.max(d));
         }
+        new_ranges.push((a, b));
     }
 
     let mut beacons = sensors
@@ -110,55 +89,66 @@ pub fn part_two(input: &str) -> Option<u64> {
         .map(|((x, y), (bx, by))| (bx - x).abs() + (by - y).abs())
         .collect_vec();
 
-    let max_xy = if sensors.len() < 15 { 20 } else { 4000000 };
-    let min_xy = 0;
+    let relevant_sensors = sensors
+        .iter()
+        .zip(distances.iter())
+        .cartesian_product(sensors.iter().zip(distances.iter()))
+        .filter(|((((x, y), _), dist1), (((a, b), _), dist2))| {
+            (x - a).abs() + (y - b).abs() == *dist1 + *dist2 + 2
+        })
+        .map(|(a, _)| a)
+        .collect_vec();
 
-    for target_y in min_xy..=max_xy {
-        let ranges = sensors
-            .iter()
-            .zip(distances.iter())
-            .filter(|(((_, y), _), dist)| (target_y - y).abs() < **dist)
-            .map(|(((x, y), _), dist)| {
-                let range = *dist - (target_y - y).abs();
-                (x - range, x + range)
-            })
-            .collect_vec();
+    let points = relevant_sensors
+        .into_iter()
+        .flat_map(|(((x, y), _), dist)| {
+            vec![(x - y + dist, x + y + dist), (x - y - dist, x + y - dist)]
+        })
+        .collect_vec();
 
-        let mut new_ranges = vec![];
+    let mut left = points
+        .iter()
+        .cartesian_product(points.iter())
+        .filter(|((x, _), (a, _))| (x - a).abs() == 2)
+        .map(|((x, _), (a, _))| (x + a) / 2)
+        .collect_vec();
 
-        for (mut a, mut b) in ranges {
-            while let Some(index) = new_ranges
+    let mut right = points
+        .iter()
+        .cartesian_product(points.iter())
+        .filter(|((_, y), (_, b))| (y - b).abs() == 2)
+        .map(|((_, y), (_, b))| (y + b) / 2)
+        .collect_vec();
+
+    left.sort();
+    left.dedup();
+    right.sort();
+    right.dedup();
+
+    let mut candidates = left
+        .into_iter()
+        .cartesian_product(right.into_iter())
+        .map(|(left, right)| ((left + right) / 2, (right - left) / 2))
+        .collect_vec();
+
+    if candidates.len() == 1 {
+        let (x, y) = candidates[0];
+        Some(x as u64 * 4000000 + y as u64)
+    } else {
+        let max_xy = if sensors.len() < 15 { 20 } else { 4000000 };
+
+        candidates.retain(|(x, y)| *x >= 0 && *y >= 0 && *x <= max_xy && *y <= max_xy);
+        candidates.retain(|(x, y)| {
+            sensors
                 .iter()
-                .position(|(c, d)| (a <= *c && b >= *c - 1) || (*c <= a && *d >= a - 1))
-            {
-                let (c, d) = new_ranges.remove(index);
-                (a, b) = (a.min(c), b.max(d));
-            }
-            new_ranges.push((a, b));
-        }
+                .map(|(a, _)| a)
+                .zip(distances.iter())
+                .all(|((a, b), dist)| (x - a).abs() + (y - b).abs() > *dist)
+        });
 
-        if new_ranges.len() > 1 {
-            assert!(new_ranges.len() == 2);
-
-            let (a, b) = new_ranges[0];
-            let (c, d) = new_ranges[1];
-
-            let x = if a + 2 == d {
-                a + 1
-            } else if a == d + 2 {
-                d + 1
-            } else if b + 2 == c {
-                b + 1
-            } else {
-                c + 1
-            };
-            let y = target_y;
-
-            return Some(x as u64 * 4000000 + y as u64);
-        }
+        let (x, y) = candidates[0];
+        Some(x as u64 * 4000000 + y as u64)
     }
-
-    unreachable!()
 }
 
 fn main() {
